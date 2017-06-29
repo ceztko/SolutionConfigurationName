dee12d0eb2abd997c3b2ddd96041d74ce842c0dd
Francesco Pretto <ceztko@gmail.com>
2017-06-29 22:52:17 +0200

    Added .NET tests

---
+++ Resources/Test/ConsoleAppNet/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            c.Foo();
+        }
+    }
+}
